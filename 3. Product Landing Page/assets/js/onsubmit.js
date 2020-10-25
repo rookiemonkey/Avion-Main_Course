0c1205f0b0e01a723fac4cabd00a6770f445e52f
rookiemonkey <kevinroirigorbasina@pm.me>
2020-10-25 22:23:04 +0800

    implemented contact form with email server

--- 3. Product Landing Page/assets/js/onsubmit.js
+++ 3. Product Landing Page/assets/js/onsubmit.js
@@ -1,34 +1,72 @@
-function handleSubmit(event) {
-    // prevents the page from reloading
-    event.preventDefault();
+async function handleSubmit(event) {
+    try {
+        // prevents the page from reloading
+        event.preventDefault();
 
-    // validate the input, returns bool
-    const areValid = validator.validate();
+        // validate the input, returns bool
+        const areValid = validator.validate();
 
-    if (!areValid) {
-        // halt the function execution
-        return null;
-    }
+        if (!areValid) {
+            // halt the function execution, no need of error message
+            throw new Error();
+        }
 
-    // selects the needed DOM elements
-    const loader = document.querySelector('section.loader');
-    const message = document.querySelector('section.submitted');
+        // selects the needed DOM elements
+        const loader = document.querySelector('section.loader');
+        const message = document.querySelector('section.submitted');
+        const form = document.querySelector('#form');
 
-    // remove the form from the DOM
-    document.querySelector('#form').remove();
+        // extract the form data
+        const formdata = new FormData(form);
 
-    // remove the hide class for loader
-    loader.classList.remove('hide');
+        // hide the form
+        form.classList.add('hide');
 
-    // remove the hide class to reveal the thank you message after 2s delay
-    // and hide the loader again
-    setTimeout(() => {
+        // initialize an emtpy object
+        let data = {};
+
+        // form an object using the array
+        [...formdata].forEach(formitem => {
+            data[formitem[0]] = formitem[1]
+        });
+
+        // remove the hide class for loader to show it
+        loader.classList.remove('hide');
+
+        // proceed with the HTTP request
+        const emailserver = 'https://krrb-prod-emailserver-p-3.herokuapp.com/email';
+
+        const raw = await fetch(emailserver, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+        // check the response status code
+        if (raw.status !== 200) {
+            throw new Error('Something went wrong upon with the email server. Please try again later')
+        }
+
+        // if 200, remove the hide class to reveal the thank you message
+        // and hide the loader again
         loader.classList.add('hide');
         message.classList.remove('hide');
-    }, 2000)
 
-    // set to the user's browser that they already submitted a feedback
-    // unless they clear the cache of their browser
-    localStorage.setItem('submitted', 'true');
+        // set to the user's browser that they already submitted a feedback
+        // unless they clear the cache of their browser
+        localStorage.setItem('submitted', 'true');
+    }
 
+    catch (error) {
+        const loader = document.querySelector('section.loader');
+        const errormessage = document.querySelector('section.error');
+
+        // hide the loader again
+        loader.classList.add('hide');
+
+        // show the error
+        errormessage.classList.remove('hide');
+    }
 }
