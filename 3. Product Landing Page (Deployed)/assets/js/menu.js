996ca0fa7707012201719d8d79c643d41836dba2
rookiemonkey <kevinroirigorbasina@pm.me>
2020-10-29 17:07:45 +0800

    tweaked the layout for visible sidebar

--- 3. Product Landing Page (Deployed)/assets/js/menu.js
+++ 3. Product Landing Page (Deployed)/assets/js/menu.js
@@ -1,4 +1,5 @@
 const Body = document.querySelector('body');
+const Body_Content = document.querySelector('#main_content');
 const Menu = document.querySelector('#menu');
 const Menu_Button = document.querySelector('#menu_button');
 const Munu_Button_Close = document.querySelector('#close_sidebar_btn');
@@ -9,12 +10,14 @@ Body.insertAdjacentElement('beforeend', Menu);
 Menu_Button.addEventListener('click', function (event) {
     event.stopPropagation();
     Body.classList.add('is-menu-visible');
+    Body_Content.classList.add('shifted_to_left');
 })
 
 // click listener for the close sidebar button
 Munu_Button_Close.addEventListener('click', function (event) {
     event.stopPropagation();
     Body.classList.remove('is-menu-visible');
+    Body_Content.classList.remove('shifted_to_left');
 })
 
 // click listener for the body to close only if the side bar is open
@@ -22,5 +25,6 @@ Body.addEventListener('click', function (event) {
     event.stopPropagation();
     if (this.className == 'is-menu-visible') {
         this.classList.remove('is-menu-visible')
+        Body_Content.classList.remove('shifted_to_left');
     }
 })
