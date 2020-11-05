19eefe058603a54d3f35a69c58da9527fb2cf6e9
rookiemonkey <kevinroirigorbasina@pm.me>
2020-11-05 22:30:38 +0800

    tweaked styles

--- 4. Avion School Landing Page (Deployed)/assets/js/menu.js
+++ 4. Avion School Landing Page (Deployed)/assets/js/menu.js
@@ -5,3 +5,16 @@ document
 document
     .querySelector('#close_sidebar_btn')
     .addEventListener('click', toggle_menu); // refer to functions.js
+
+
+/**
+ * attach an onclick listeners to every sidebar links so that
+ * every time a sidebar link is clicked, side bar will automatically close
+ * except for 'enroll now' button
+ */
+
+const sidebar_links = document.querySelectorAll('.sidebar-link');
+
+sidebar_links.forEach(function (link) {
+    link.addEventListener('click', toggle_menu);
+});
