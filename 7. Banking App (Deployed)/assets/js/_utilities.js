3dae562fe299647d4c8feb5009ad2b5d586b3a0d
rookiemonkey <kevinroirigorbasina@pm.me>
2020-12-06 13:19:13 +0800

    tweaked styles/scripts

---
+++ 7. Banking App (Deployed)/assets/js/_utilities.js
@@ -0,0 +1,29 @@
+// ====================================================== //
+// UTILITY FUNCTIONS 
+// ====================================================== //
+function parseFormData(formData) {
+    const formdataParsed = {};
+
+    [...formData].forEach(formitem => {
+        formdataParsed[formitem[0]] = formitem[1]
+    });
+
+    return formdataParsed;
+}
+
+function resetForms() {
+    BankingApp.form_register.reset();
+    BankingApp.form_login.reset();
+    BankingApp.form_deposit.reset();
+    BankingApp.form_withdraw.reset();
+    BankingApp.form_send.reset();
+    BankingApp.changeAvatarBtn.value = '';
+    BankingApp.transactionSelect.value = 'All';
+    BankingApp.transactionsList.innerHTML = '';
+}
+
+function withCommas(num) {
+    return num
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
