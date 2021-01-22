b0f3ebac327b483e2e41ecdfaf9071e448f31dda
rookiemonkey <kevinroirigorbasina@pm.me>
2021-01-22 20:48:31 +0800

    implemented some from scratch :tada:

--- __Ruby_Exercises/012221/1.rb
+++ __Ruby_Exercises/012221/1.rb
@@ -1,6 +1,21 @@
-
 arr = [1, 3, 5, 7, 9, 11]
 number = 3
+
+
+# FROM SCRATCH
+def custom_include(arr, num)
+    is_included = false
+    arr.each { |n| is_included = true if n == num }
+    is_included
+end
+
+p custom_include arr, number
+p custom_include arr, 152
+
+
+
+
+# USING NATIVE METHOD
 
 p arr.include? number
 p arr.include? 152
--- __Ruby_Exercises/012221/4.rb
+++ __Ruby_Exercises/012221/4.rb
@@ -1,6 +1,22 @@
 
 arr = [6, 3, 1, 8, 4, 2, 10, 65, 102]
 
+# FROM SCRATCH
+
+scratch_array = Array.new
+
+arr.each do |n|
+    scratch_array << n if n % 2 == 0
+end
+
+p 'SCRATCH: ', scratch_array
+
+
+
+
+
+# USING NATIVE METHOD
+
 new_array = arr.select { |n| n.even? }
 
-p new_array
+p 'NATIVE: ', new_array
